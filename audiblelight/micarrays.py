fb196ef9982abae55f82f2fbc7c6d090c556f249
Huw Cheston <hwc31@cam.ac.uk>
2025-06-17 13:40:48 +0100

    initial commit to branch

---
+++ audiblelight/micarrays.py
@@ -0,0 +1,465 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Implements dataclasses for working with common microphone array types"""
+
+from dataclasses import dataclass
+
+import numpy as np
+
+from audiblelight import utils
+
+__all__ = [
+    "polar_to_cartesian",
+    "cartesian_to_polar",
+    "center_coordinates",
+    "MicArray",
+    "Eigenmike32",
+    "Eigenmike64",
+    "AmbeoVR",
+    "DeccaCuboid",
+    "Oct3D",
+    "PCMA3D",
+    "Cube2L",
+    "HamasakiSquare"
+]
+
+
+def polar_to_cartesian(spherical_array: np.ndarray) -> np.ndarray:
+    """Converts an array of spherical coordinates (azimuth°, polar°, radius) to Cartesian coordinates (XYZ)."""
+    spherical_array = utils.coerce2d(spherical_array)
+    # Convert azimuth + elevation to radians
+    azimuth_rad = np.deg2rad(spherical_array[:, 0])  # phi
+    polar_rad = np.deg2rad(spherical_array[:, 1])    # theta, polar angle from z-axis
+    # No need to do this for radius
+    r = spherical_array[:, 2]
+    # Express everything in cartesian form
+    x = r * np.sin(polar_rad) * np.cos(azimuth_rad)
+    y = r * np.sin(polar_rad) * np.sin(azimuth_rad)
+    z = r * np.cos(polar_rad)
+    # Stack into a 2D array of shape (n_capsules, 3)
+    return np.column_stack((x, y, z))
+
+
+def cartesian_to_polar(cartesian_array: np.ndarray) -> np.ndarray:
+    """Converts an array of Cartesian coordinates (XYZ) to spherical coordinates (azimuth°, polar°, radius)."""
+    cartesian_array = utils.coerce2d(cartesian_array)
+    # Unpack everything
+    x = cartesian_array[:, 0]
+    y = cartesian_array[:, 1]
+    z = cartesian_array[:, 2]
+    # Compute radius using the classic equation
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    assert r > 0, f"Expected radius > 0, but got radius = {r}"
+    # Get azimuth and polar in radians first, then convert to degrees
+    azimuth = np.rad2deg(np.arctan2(y, x))  # φ, angle in x-y plane from x-axis
+    polar = np.rad2deg(np.arccos(z / r))  # θ, angle from z-axis
+    # Ensure azimuth is in [0, 360)
+    azimuth = np.mod(azimuth, 360)
+    # Stack everything back into a 2D array of shape (n_capsules, 3)
+    return np.column_stack((azimuth, polar, r))
+
+
+def center_coordinates(cartesian_array: np.ndarray) -> np.ndarray:
+    """Take a dictionary of cartesian coordinates, find the center, and subtract to center all coordinates around 0"""
+    # Shape (3,)
+    c_mean = np.mean(cartesian_array, axis=0)
+    # Shape (n_capsules, 3)
+    return cartesian_array - c_mean
+
+
+@dataclass
+class MicArray:
+    """
+    This is the base class for all microphone array types.
+
+    Attributes:
+        name (str): the name of the array.
+        is_spherical (bool): whether the array is spherical. If False, positions_spherical will be None.
+
+    Properties:
+        coordinates_polar (np.array): the positions of the capsules on the array, given as azimuth, colatitude, radius
+            (i.e., degrees, degrees, meters). Azimuth is measured counter-clockwise, where 0 == the front of the
+            microphone (as given, e.g., by the manufacturer logo), colatitude/elevation increases from the top
+            (0, away from the shaft) to the bottom (180, aligned with the shaft). When `is_spherical` is False, is None.
+        coordinates_cartesian (np.array): the positions of the capsules in Cartesian (XYZ) coordinates, with distance
+            measured using meters away from the center of the array (nb: coordinates
+        n_capsules (int): number of capsules in the array
+        capsule_names (list[str]): the names of the microphone capsules
+    """
+
+    name: str = ""
+    is_spherical: bool = False
+    
+    @property
+    def coordinates_polar(self) -> np.ndarray:
+        raise NotImplementedError
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        raise NotImplementedError
+
+    @property
+    def n_capsules(self) -> int:
+        return len(self.capsule_names)
+    
+    @property
+    def capsule_names(self) -> list[str]:
+        return []
+
+    def coordinates_absolute(self, mic_center: np.ndarray) -> np.ndarray:
+        """
+        Calculates absolute position of all microphone capsules based on a provided center.
+
+        The center should be in cartesian coordinates with the form (XYZ), with units in meters.
+        """
+        mic_center = utils.coerce2d(mic_center)
+        return self.coordinates_cartesian + mic_center
+
+    def __len__(self) -> int:
+        return self.n_capsules
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return f"Microphone array {self} with {len(self)} capsules"
+
+
+@dataclass
+class AmbeoVR(MicArray):
+    """
+    Sennheiser AmbeoVR microphone.
+
+    Adapted from https://github.com/micarraylib/micarraylib/blob/main/micarraylib/arraycoords/array_shapes_raw.py
+    """
+
+    name: str = "ambeovr"
+    is_spherical: bool = True
+
+    @property
+    def coordinates_polar(self) -> np.ndarray:
+        return np.array([
+            [45, 55, 0.01],
+            [315, 125, 0.01],
+            [135, 125, 0.01],
+            [225, 55, 0.01]
+        ])
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        """The positions of the capsules in Cartesian coordinates, i.e. as meters from the center of the array."""
+        return polar_to_cartesian(self.coordinates_polar)
+    
+    @property
+    def capsule_names(self) -> list[str]:
+        return ["FLU", "FRD", "BLD", "BRU"]
+
+
+@dataclass
+class Eigenmike32(MicArray):
+    """
+    Eigenmike 32 microphone.
+
+    Adapted from https://eigenmike.com/sites/default/files/documentation-2023-10/EigenStudio%20User%20Manual%20R02D.pdf
+    """
+
+    name: str = "eigenmike32"
+    is_spherical: bool = True
+
+    @property
+    def coordinates_polar(self) -> np.ndarray:
+        return np.array([
+            [0,    69, 0.042],
+            [32,   90, 0.042],
+            [0,   111, 0.042],
+            [328,  90, 0.042],
+            [0,    32, 0.042],
+            [45,   55, 0.042],
+            [69,   90, 0.042],
+            [45,  125, 0.042],
+            [0,   148, 0.042],
+            [315, 125, 0.042],
+            [291,  90, 0.042],
+            [315,  55, 0.042],
+            [91,   21, 0.042],
+            [90,   58, 0.042],
+            [90,  121, 0.042],
+            [89,  159, 0.042],
+            [180,  69, 0.042],
+            [212,  90, 0.042],
+            [180, 111, 0.042],
+            [148,  90, 0.042],
+            [180,  32, 0.042],
+            [225,  55, 0.042],
+            [249,  90, 0.042],
+            [225, 125, 0.042],
+            [180, 148, 0.042],
+            [135, 125, 0.042],
+            [111,  90, 0.042],
+            [135,  55, 0.042],
+            [269,  21, 0.042],
+            [270,  58, 0.042],
+            [270, 122, 0.042],
+            [271, 159, 0.042]
+        ])
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        """The positions of the capsules in Cartesian coordinates, i.e. as meters from the center of the array."""
+        return polar_to_cartesian(self.coordinates_polar)
+    
+    @property
+    def capsule_names(self) -> list[str]:
+        return [str(i) for i in range(1, 33)]
+
+
+@dataclass
+class Eigenmike64(MicArray):
+    """
+    Eigenmike 64 microphone.
+
+    Adapted from https://eigenmike.com/sites/default/files/documentation-2024-09/getting%20started%20Guide%20to%20em64%20and%20ES3%20R01H.pdf
+    """
+
+    name: str = "eigenmike64"
+    is_spherical: bool = True
+
+    @property
+    def coordinates_polar(self) -> np.ndarray:
+        # TODO: looks weird
+        return np.array([
+            [197, 17, 0.042],
+            [116, 22, 0.041],
+            [82, 42, 0.041],
+            [313, 13, 0.041],
+            [43, 23, 0.039],
+            [47, 53, 0.035],
+            [336, 38, 0.041],
+            [15, 43, 0.036],
+            [204, 46, 0.041],
+            [207, 70, 0.039],
+            [247, 33, 0.041],
+            [234, 60, 0.041],
+            [265, 56, 0.040],
+            [100, 67, 0.041],
+            [105, 93, 0.041],
+            [121, 48, 0.041],
+            [127, 78, 0.039],
+            [148, 62, 0.040],
+            [163, 38, 0.041],
+            [179, 64, 0.041],
+            [21, 70, 0.041],
+            [26, 96, 0.040],
+            [48, 81, 0.036],
+            [56, 106, 0.038],
+            [71, 68, 0.040],
+            [78, 92, 0.041],
+            [293, 40, 0.038],
+            [291, 41, 0.040],
+            [318, 59, 0.038],
+            [334, 82, 0.040],
+            [352, 63, 0.041],
+            [0, 90, 0.042],
+            [174, 138, 0.043],
+            [213, 140, 0.043],
+            [252, 135, 0.043],
+            [151, 109, 0.041],
+            [241, 108, 0.041],
+            [293, 142, 0.038],
+            [331, 111, 0.041],
+            [61, 109, 0.041],
+            [227, 115, 0.039],
+            [234, 86, 0.041],
+            [194, 116, 0.041],
+            [210, 95, 0.039],
+            [183, 90, 0.042],
+            [164, 111, 0.041],
+            [157, 75, 0.041],
+            [139, 80, 0.039],
+            [136, 102, 0.034],
+            [102, 53, 0.042],
+            [113, 28, 0.042],
+            [83, 27, 0.041],
+            [308, 55, 0.040],
+            [309, 0, 0.038],
+            [278, 24, 0.041],
+            [283, 49, 0.041],
+            [253, 36, 0.041],
+            [260, 61, 0.041],
+            [60, 46, 0.042],
+            [14, 54, 0.042],
+            [32, 35, 0.040],
+            [334, 46, 0.041],
+            [2, 26, 0.041],
+            [335, 35, 0.041],
+        ])
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        """The positions of the capsules in Cartesian coordinates, i.e. as meters from the center of the array."""
+        return polar_to_cartesian(self.coordinates_polar)
+    
+    @property
+    def capsule_names(self) -> list[str]:
+        return [str(i) for i in range(1, 65)]
+
+
+@dataclass
+class Oct3D(MicArray):
+    """
+    Oct3D (Optimised Cardioid Triangle) recording technique for 9 channel surround sound.
+
+    Adapted from https://schoeps.de/en/products/surround-3d/oct-3d-set/oct-3d.html
+    """
+
+    name: str = "oct3d"
+    is_spherical: bool = False
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        return center_coordinates(np.array([
+            # x, y, z
+            # (meters)
+            [0, 0.35, 0],
+            [0, -0.35, 0],
+            [0.05, 0, 0],
+            [-0.4, 0.5, 0],
+            [-0.4, -0.5, 0],
+            [0, 0.5, 1.0],
+            [0, -0.5, 1.0],
+            [-1.0, 0.5, 1.0],
+            [-1.0, -0.5, 1.0],
+        ]))
+
+    @property
+    def capsule_names(self) -> list[str]:
+        # FLh = "Front-Left-Height"
+        return ["FL", "FR", "FC", "RL", "RR", "FLh", "FRh", "RLh", "RRh"]
+
+
+@dataclass
+class PCMA3D(MicArray):
+    """
+    PCMA-3D microphone array.
+
+    The PCMA-3D array is based on the Perspective Control Microphone Array design, which incorporates five channels.
+    """
+
+    name: str = "pcma3d"
+    is_spherical: bool = False
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        return center_coordinates(np.array([
+            [0, 0.5, 0],
+            [0, -0.5, 0],
+            [0.25, 0, 0],
+            [-1.0, 0.5, 0],
+            [-1.0, -0.5, 0],
+            [0.0, 0.5, 0.0],
+            [0.0, -0.5, 0.0],
+            [-1.0, 0.5, 0.0],
+            [-1.0, -0.5, 0.0],
+        ]))
+    
+    @property
+    def capsule_names(self) -> list[str]:
+        return ["FL", "FR", "FC", "RL", "RR", "FLh", "FRh", "RLh", "RRh"]
+
+
+@dataclass
+class Cube2L(MicArray):
+    """
+    2L-Cube microphone array.
+
+    The 2L-Cube employs 9 microphones in a cuboid arrangement. The dimensions in the initial description of this array
+    are unclear, so here we implement a 1 x 1 x 1 meter cube, for comparison with the PCMA-3D array.
+    """
+
+    name: str = "cube2l"
+    is_spherical: bool = False
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        return center_coordinates(np.array([
+            [0, 0.5, 0],
+            [0, -0.5, 0],
+            [0.25, 0, 0],
+            [-1.0, 0.5, 0],
+            [-1.0, -0.5, 0],
+            [0, 0.5, 1.0],
+            [0, -0.5, 1.0],
+            [-1.0, 0.5, 1.0],
+            [-1.0, -0.5, 1.0],
+        ]))
+    
+    @property
+    def capsule_names(self) -> list[str]:
+        return ["FL", "FR", "FC", "RL", "RR", "FLh", "FRh", "RLh", "RRh"]
+
+
+@dataclass
+class DeccaCuboid(MicArray):
+    """
+    Decca Cuboid microphone array.
+
+    The Decca Cuboid is based on the Decca Tree, a widely used array setup for Orchestral recordings. The extensions
+    consist of placing rear microphones behind the base point and height microphones above the base layer.
+    """
+
+    name: str = "deccacuboid"
+    is_spherical: bool = False
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        return center_coordinates(np.array([
+            [0, 1.0, 0],
+            [0, -1.0, 0],
+            [0.25, 0, 0],
+            [-2.0, 1.0, 0],
+            [-2.0, -1.0, 0],
+            [0, 1.0, 1.0],
+            [0, -1.0, 1.0],
+            [-2.0, 1.0, 1.0],
+            [-2.0, -1.0, 1.0],
+        ]))
+    
+    @property
+    def capsule_names(self) -> list[str]:
+        return ["FL", "FR", "FC", "RL", "RR", "FLh", "FRh", "RLh", "RRh"]
+
+
+@dataclass
+class HamasakiSquare(MicArray):
+    """
+    Hamasaki Square With Height microphone array.
+
+    The Hamasaki Square is a popular technique for recording four-channel ambience. This array is a vertical extension
+    of the approach, consisting of 12 channels.
+    """
+
+    name: str = "hamasakisquare"
+    is_spherical: bool = False
+
+    @property
+    def coordinates_cartesian(self) -> np.ndarray:
+        return center_coordinates(np.array([
+            [0, 1.0, 0],
+            [0, -1.0, 0],
+            [-2.0, 1.0, 1.6],
+            [-2.0, -1.0, 1.6],
+            [0.0, 1.0, 0.0],
+            [0.0, -1.0, 0.0],
+            [-2.0, 1.0, 1.6],
+            [-2.0, -1.0, 1.6],
+            [0, 1.0, 1.0],
+            [0, -1.0, 1.0],
+            [-2.0, 1.0, 2.6],
+            [-2.0, -1.0, 2.6],
+        ]))
+
+    @property
+    def capsule_names(self) -> list[str]:
+        return ["FL", "FR", "RL", "RR", "FLh_0", "FRh_0", "RLh_0", "RRh_0", "FLh_1", "FRh_1", "RLh_1", "RRh_1"]
+    
