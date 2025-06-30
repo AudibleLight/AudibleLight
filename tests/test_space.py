68849c18510f0f329144c045dbc5cd98e2194392
Huw Cheston <hwc31@cam.ac.uk>
2025-06-30 18:05:21 +0100

    fix unittests

--- tests/test_space.py
+++ tests/test_space.py
@@ -9,8 +9,9 @@ import pytest
 import numpy as np
 from trimesh import Trimesh
 
-from audiblelight.space import Space, load_mesh, repair_mesh
 from audiblelight import utils
+from audiblelight.space import Space, load_mesh, repair_mesh
+from audiblelight.micarrays import MICARRAY_LIST
 
 
 TEST_RESOURCES = utils.get_project_root() / "tests/test_resources"
@@ -46,11 +47,10 @@ def test_load_broken_mesh(mesh_fpath: str, expected):
 
 @pytest.fixture(scope="module")
 def oyens_space() -> Space:
-    """Returns a Space object with the Oyens mesh (Gibson) and a single microphone"""
+    """Returns a Space object with the Oyens mesh (Gibson)"""
     oyens = os.path.join(utils.get_project_root(), "tests/test_resources/Oyens.glb")
     space = Space(
         oyens,
-        mic_positions=[-0.5, -0.5, 0.5],
         min_distance_from_source=0.2,    # all in meters
         min_distance_from_mic=0.1,    # all in meters
         min_distance_from_surface=0.2    # all in meters
@@ -59,22 +59,36 @@ def oyens_space() -> Space:
 
 
 @pytest.mark.parametrize(
-    "microphone_positions",
+    "microphones,expected_shape",
     [
-        np.array([[-0.5, -0.5, 0.5,]]),    # 1 mic setup
-        np.array([[-0.1, -0.1, 0.6], [0.5, 0.5, 0.5]])     # 2 mic setup
+        (None, 1),    # places 1 random mic in a random position
+        (3, 3),    # places 3 random mics in 3 random positions
+        ("eigenmike32", 1),    # places eigenmike32 in random position
+        (["ambeovr", "ambeovr"], 2),     # places two ambeoVRs in two random positions
+        ({"eigenmike32": [-0.5, -0.5, 0.5]}, 1),    # places eigenmike32 in assigned position
+        ({"eigenmike32": [-0.5, -0.5, 0.5], "ambeovr": [-0.1, -0.1, 0.6]}, 2),    # places mics in assigned positions
+        ([[-0.5, -0.5, 0.5], [-0.1, -0.1, 0.6]], 2)    # places two random mics in two assigned positions
     ]
 )
-def test_place_microphones(microphone_positions):
-    space = Space(TEST_MESHES[0], microphone_positions)
-    # Should have the same number of mic positions we've passed in
-    assert isinstance(space.mic_positions, np.ndarray)
+def test_place_microphones(microphones, expected_shape, oyens_space: Space):
+    # Add the microphones to the space: keep_existing=False ensures we remove previously-added microphones
+    oyens_space.add_microphones(microphones=microphones, keep_existing=False)
     # All mic positions should have 3D coordinates
-    assert space.mic_positions.shape == (len(microphone_positions), 3)
+    assert len(oyens_space.microphones) == expected_shape
+    # Should have exactly 1 listener for every microphone capsule
+    n_capsules = sum([m.n_capsules for m in oyens_space.microphones])
+    assert n_capsules == oyens_space.ctx.get_listener_count()
     # Iterate over all mics
-    for mic_position in space.mic_positions:
-        # Should be inside the mesh
-        assert space._is_point_inside_mesh(mic_position)
+    valid_mics = [ma().name for ma in MICARRAY_LIST]
+    for mic_idx, mic in enumerate(oyens_space.microphones):
+        # Microphone array type should be valid
+        assert mic.name in valid_mics
+        # Microphones should have coordinates assigned to them
+        assert mic.coordinates_absolute is not None
+        assert mic.coordinates_center is not None
+        # Iterate over all capsules
+        for capsule in mic.coordinates_absolute:
+            assert oyens_space._is_point_inside_mesh(capsule)
 
 
 @pytest.mark.parametrize(
@@ -90,6 +104,7 @@ def test_place_microphones(microphone_positions):
 )
 def test_validate_source_positions(test_position: np.ndarray, expected: bool, oyens_space: Space):
     """Given a microphone with coordinates [-0.5, -0.5, 0.5], test whether test_position is valid"""
+    oyens_space.add_microphones(microphones={"ambeovr": [-0.5, -0.5, 0.5]}, keep_existing=False)
     assert oyens_space._validate_source_position(test_position) == expected
 
 
@@ -101,6 +116,7 @@ def test_validate_source_positions(test_position: np.ndarray, expected: bool, oy
     ]
 )
 def test_add_sources(test_position: np.ndarray, expected_shape: tuple[int], oyens_space: Space):
+    oyens_space.add_microphones(microphones={"ambeovr": [-0.5, -0.5, 0.5]}, keep_existing=False)
     # Add the sources in and check that the shape of the resulting array is what we expect
     oyens_space.add_sources(test_position)
     assert oyens_space.source_positions.shape == expected_shape
@@ -115,6 +131,7 @@ def test_add_sources(test_position: np.ndarray, expected_shape: tuple[int], oyen
     ]
 )
 def test_add_sources_relative_to_mic(test_position: np.ndarray, expected_shape: tuple[int], oyens_space: Space):
+    oyens_space.add_microphones(microphones={"ambeovr": [-0.5, -0.5, 0.5]}, keep_existing=False)
     # Add the sources in and check that the shape of the resulting array is what we expect
     oyens_space.add_sources_relative_to_mic(test_position, 0)
     assert oyens_space.source_positions.shape == expected_shape
@@ -131,10 +148,12 @@ def test_calculate_weighted_average_ray_length(num_rays: int, oyens_space: Space
     assert result > 0
 
 
-@pytest.mark.parametrize("test_num", range(5))
+@pytest.mark.parametrize("test_num", range(1, 5))
 def test_get_random_position(test_num: int, oyens_space: Space):
     # For reproducible results
     utils.seed_everything(test_num)
+    # Add some microphones to the space
+    oyens_space.add_microphones(test_num, keep_existing=False)
     # Add some sources to the space
     oyens_space.add_random_sources(test_num)
     # Grab a random position
@@ -144,20 +163,30 @@ def test_get_random_position(test_num: int, oyens_space: Space):
     assert random_point.shape == (3,)   # should be a 1D array of XYZ
 
 
-@pytest.mark.parametrize("n_mics,n_sources", [(m, s) for m, s in zip(list(range(1, 20, 5))[::-1], range(1, 20, 5))])
+# Goes (1 mic, 4 sources), (2 mics, 3 sources), (3 mics, 2 sources), (4 mics, 1 source)
+@pytest.mark.parametrize("n_mics,n_sources", [(m, s) for m, s in zip(list(range(1, 5))[::-1], range(1, 5))])
 def test_simulated_ir_shape(n_mics: int, n_sources: int, oyens_space: Space):
     # For reproducible results
     utils.seed_everything(n_sources)
-    # Add some sources to the space
+    # Add some sources and ambeovr microphones to the space
+    #  We could use other microphone types but they're slow to simulate
+    oyens_space.add_microphones(["ambeovr" for _ in range(n_mics)], keep_existing=False)
     oyens_space.add_random_sources(n_sources)
-    # Grab the IRs, should be in shape (N_mics, N_sources, N_channels, N_samples)
+    # Grab the IRs: we should have one array for every microphone
     simulated_irs = oyens_space.simulate()
-    # Get the shapes of all arrays
-    expected_mics, expected_sources, expected_channels = (1, n_sources, 4)
-    actual_mics, actual_sources, actual_channels, actual_samples = simulated_irs.shape
-    # Compare across the first 3 dims of the IR array
-    assert expected_mics == actual_mics
-    assert expected_sources == actual_sources
-    assert expected_channels == actual_channels
-    # We can't easily check the number of samples explicitly, but it should be more than 1
-    assert actual_samples >= 1
+    assert len(simulated_irs) == n_mics
+    # Iterate over each individual microphone
+    total_capsules = 0
+    for mic in oyens_space.microphones:
+        # Grab the shape of the IRs for this microphone
+        actual_capsules, actual_sources, actual_samples = mic.irs.shape
+        # We should have the expected number of sources, capsules, and samples
+        assert actual_sources == n_sources
+        assert actual_capsules == mic.n_capsules
+        assert actual_samples >= 1    # difficult to test number of samples
+        total_capsules += actual_capsules
+    # IRs for all microphones should have same number of sources and samples
+    assert all([m.irs.shape[1] == oyens_space.microphones[0].irs.shape[1] for m in oyens_space.microphones]) # sources
+    assert all([m.irs.shape[2] == oyens_space.microphones[0].irs.shape[2] for m in oyens_space.microphones]) # samples
+    # Number of capsules should be the same as the "raw" results of the raytracing engine
+    assert total_capsules == oyens_space.ctx.get_audio().shape[0]
