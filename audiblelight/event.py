2140ff5a0d64743ee331eaacb6057217dd841e50
huwcheston <hwc31@cam.ac.uk>
2025-07-21 17:49:47 +0100

    updates to loading audio for `Event` class + tests

--- audiblelight/event.py
+++ audiblelight/event.py
@@ -6,154 +6,216 @@
 from pathlib import Path
 from typing import Union, Optional
 
+import librosa
 import numpy as np
-import soundfile as sf
+from loguru import logger
 
 from audiblelight.worldstate import Emitter
 from audiblelight import utils
 
+# Mapping from sound events to labels used in DCASE challenge
+DCASE_SOUND_EVENT_CLASSES = {
+    "femaleSpeech": 0,
+    "maleSpeech": 1,
+    "clapping": 2,
+    "telephone": 3,
+    "laughter": 4,
+    "domesticSounds": 5,
+    "footsteps": 6,
+    "doorCupboard": 7,
+    "music": 8,
+    "musicInstrument": 9,
+    "waterTap": 10,
+    "bell": 11,
+    "knock": 12,
+}
+_DCASE_SOUND_EVENT_CLASSES_INV = {v: k for v, k in DCASE_SOUND_EVENT_CLASSES.items()}
 
-# class Event_:
-#     """
-#     Represents a single audio event taking place inside a Scene.
-#     """
-#     def __init__(
-#             self,
-#             filepath: Union[str, Path],
-#             start_timepoint: float,
-#             duration: float,
-#             snr: float,
-#             emitters: list[Emitter],
-#             spatial_resolution: Optional = None,
-#             spatial_velocity: Optional = None,
-#             class_id: Optional[int] = None,
-#             label: Optional[str] = "",
-#     ):
-#         pass
+
+def _infer_dcase_class_id_labels(
+    class_id: Optional[int],
+    class_label: Optional[str]
+) -> tuple[Optional[int], Optional[str]]:
+    """
+    Infers missing DCASE class ID or label if only one is provided.
+
+    - If only class_label is provided, class_id is inferred.
+    - If only class_id is provided, class_label is inferred.
+    - If both are provided or both are None, returns them as-is.
+    """
+    if class_id is None and class_label is not None:
+        class_id = DCASE_SOUND_EVENT_CLASSES.get(class_label)
+
+    elif class_id is not None and class_label is None:
+        class_label = _DCASE_SOUND_EVENT_CLASSES_INV.get(class_id)
+
+    return class_id, class_label
 
 
 class Event:
     """
     Represents a single audio event taking place inside a Scene.
     """
+
     def __init__(
             self,
             filepath: Union[str, Path],
-            start_timepoint: float,
-            duration: float,
-            snr: float,
-            start_coordinates_absolute: np.ndarray,
-            start_coordinates_relative: dict[str, np.ndarray],
+            alias: str,
+            emitters: list[Emitter],
+            scene_start: Optional[float] = 0.,
+            event_start: Optional[float] = 0.,
+            duration: Optional[float] = None,
+            snr: Optional[float] = None,
             sample_rate: Optional[int] = utils.SAMPLE_RATE,
-            is_moving: Optional[bool] = False,
-            end_coordinates_absolute: Optional[np.ndarray] = None,
-            end_coordinates_relative: Optional[dict[str, np.ndarray]] = None,
-            spatial_resolution: Optional = None,
-            spatial_velocity: Optional = None,
             class_id: Optional[int] = None,
-            label: Optional[str] = "",
+            class_label: Optional[str] = None,
+            spatial_resolution: Optional[Union[int, float]] = None,
+            spatial_velocity: Optional[Union[int, float]] = None,
     ):
         """
         Initializes the Event object, representing a single audio event taking place inside a Scene.
 
         Args:
             filepath: Path to the audio file.
-            start_timepoint: Time in seconds within the Scene.
-            duration: Event duration in seconds
+            alias: Label to refer to this Event by inside the Scene
+            emitters: List of Emitter objects associated with this event.
+            scene_start: Time to start the Event within the Scene, in seconds. Must be a positive number.
+                If not provided, defaults to the beginning of the Scene (i.e., 0 seconds).
+            event_start: Time to start the Event audio from, in seconds. Must be a positive number.
+                If not provided, defaults to starting the audio at the very beginning (i.e., 0 seconds).
+            duration: Time the Event audio lasts in seconds. Must be a positive number.
+                If None or greater than the duration of the audio, defaults to using the full duration of the audio.
             snr: Signal to noise ratio for the audio file with respect to the noise floor
-            start_coordinates_absolute: Position of the Event within the Space in absolute Cartesian coordinates
-            start_coordinates_relative: Cartesian coordinates of the Event with respect to the microphones in the Space
-            is_moving (optional): Whether the Event is moving or not, defaults to False (i.e., static event)
-
+            sample_rate: If not None, the audio will be resampled to the given sample rate.
+            class_label: Optional label to use for sound event class.
+                If not provided, the label will attempt to be inferred from the ID using the DCASE sound event classes.
+            class_id: Optional ID to use for sound event class.
+                If not provided, the ID will attempt to be inferred from the label using the DCASE sound event classes.
         """
         # Setting attributes for audio
-        self.filepath = utils.sanitise_filepath(filepath)    # will raise an error if not found on disk
+        self.filepath = utils.sanitise_filepath(filepath)    # will raise an error if not found on disk, coerces to Path
         self.audio = None    # will be loaded when calling `load_audio` for the first time
         self._audio_loaded = False
         self.snr = snr
         self.sample_rate = utils.sanitise_positive_number(sample_rate)
+        self.alias = alias
+
+        # Spatial attributes
+        self.spatial_resolution = spatial_resolution
+        self.spatial_velocity = spatial_velocity
+
         # Metadata attributes
         self.filename = self.filepath.name
-        self.class_id = class_id
-        self.label = label
+        #  Attempt to infer class ID and labels in cases where only one is provided
+        self.class_id, self.class_label = _infer_dcase_class_id_labels(class_id, class_label)
 
         # Setting start, duration times
+        self._audio_full_duration = librosa.get_duration(path=self.filepath)
         #  The validation functions raise an error if non-numeric or not positive
-        self.start_timepoint = utils.sanitise_positive_number(start_timepoint)
-        self.duration = utils.sanitise_positive_number(duration)
-        self.end_timepoint = self.start_timepoint + self.duration
+        self.scene_start = utils.sanitise_positive_number(scene_start) if scene_start is not None else scene_start
+        self.event_start = utils.sanitise_positive_number(event_start) if event_start is not None else event_start
+        if duration is not None:
+            duration = utils.sanitise_positive_number(duration)
+            if duration > self._audio_full_duration:
+                logger.warning(
+                    f"Duration {duration:.2f} is longer than audio duration {self._audio_full_duration:.2f}. "
+                    f"Falling back to using audio duration."
+                )
+                duration = self._audio_full_duration
+        self.duration = duration
 
-        # Set start coordinates in absolute terms
-        #  These should already be sanitised to be located within the bounds of the mesh, be an acceptable distance
-        #  from microphone objects, etc., so the only thing we validate here is that they are the correct shape and type
-        self.start_coordinates_absolute = utils.sanitise_coordinates(start_coordinates_absolute)
-        self.start_coordinates_relative_cartesian = dict()
-        self.start_coordinates_relative_polar = dict()
-        # Iterate through the relative coordinates
-        #  This is a dictionary of microphone aliases and coordinates
-        for coords_alias, coords in start_coordinates_relative:
-            coords = utils.sanitise_coordinates(coords)
-            self.start_coordinates_relative_cartesian[coords_alias] = coords
-            self.start_coordinates_relative_polar[coords_alias] = utils.cartesian_to_polar(coords)
 
-        # Handling moving source attributes
-        assert isinstance(is_moving, bool), "Expected `is_moving` to be of type `bool`"
-        self.is_moving = is_moving
-        # If the source is moving, we should provide some of these optional attributes
+        # List of emitter objects associated with this event
+        if isinstance(emitters, Emitter):
+            emitters = [emitters]    # pad to a list
+        assert all(isinstance(em, Emitter) for em in emitters)
+        self.emitters = emitters
+        #  If more than one emitter, the sound source is moving; if only one emitter, the sound source is stationary
+        self.is_moving = len(self.emitters) > 1
+
+        # We presume that the list of emitter objects is "sorted"
+        #  i.e., that the first emitter corresponds to the start position and the last to the end
+        self.start_coordinates_absolute = self.emitters[0].coordinates_absolute
+        self.start_coordinates_relative_cartesian = self.emitters[0].coordinates_relative_cartesian
+        self.start_coordinates_relative_polar = self.emitters[0].coordinates_relative_polar
+
+        # Set the ending coordinates: if the object is not moving, these are the same as the starting coordinates.
         if self.is_moving:
-            expected_attrs = [end_coordinates_relative, end_coordinates_absolute, spatial_resolution, spatial_velocity]
-            assert all([at is not None for at in expected_attrs])
-            # Set the attributes
-            self.spatial_resolution = spatial_resolution
-            self.spatial_velocity = utils.sanitise_positive_number(spatial_velocity)
-            # Update all the coordinates
-            self.end_coordinates_absolute = utils.sanitise_coordinates(end_coordinates_absolute)
-            self.end_coordinates_relative_cartesian = dict()
-            self.end_coordinates_relative_polar = dict()
-            for coords_alias, coords in start_coordinates_relative:
-                coords = utils.sanitise_coordinates(coords)
-                self.end_coordinates_relative_cartesian[coords_alias] = coords
-                self.end_coordinates_relative_polar[coords_alias] = utils.cartesian_to_polar(coords)
-        # Otherwise, if the source is not moving, we don't need to set these attributes
+            self.end_coordinates_absolute = self.emitters[-1].coordinates_absolute
+            self.end_coordinates_relative_cartesian = self.emitters[-1].coordinates_relative_cartesian
+            self.end_coordinates_relative_polar = self.emitters[-1].coordinates_relative_polar
         else:
-            self.spatial_resolution = None
-            self.spatial_velocity = None
-            self.end_coordinates_absolute = None
-            self.end_coordinates_relative_cartesian = None
-            self.end_coordinates_relative_polar = None
+            self.end_coordinates_absolute = self.start_coordinates_absolute
+            self.end_coordinates_relative_cartesian = self.start_coordinates_relative_cartesian
+            self.end_coordinates_relative_polar = self.start_coordinates_relative_polar
 
-    def load_audio(self) -> np.ndarray:
+    def load_audio(self, ignore_cache: bool = False) -> np.ndarray:
         """
         Returns the audio array of the Event.
+
+        The audio will be loaded, resampled to the desired sample rate, converted to mono, and then truncated to match
+        the event start time and duration.
+
+        After calling this function once, `audio` is cached as an attribute of this Event instance, and this
+        attribute will be returned on successive calls unless `ignore_cache` is True.
+
+        Returns:
+            np.ndarray: the audio array.
         """
+        # Invalidate the cache if required
+        if ignore_cache:
+            self._audio_loaded = False
+            self.audio = None
+
         # If we've already loaded the audio, it should be cached and we can return straight away
         if self._audio_loaded and isinstance(self.audio, np.ndarray):
             return self.audio
-        # Otherwise, we need to load up the audio
+
         else:
-            self.audio, _ = sf.read(str(self.filepath), samplerate=self.sample_rate, always_2d=True,)
+            # Otherwise, we need to load up the audio
+            #  Using soundfile, this will resample to the desired sample rate.
+            audio, _ = librosa.load(self.filepath, sr=self.sample_rate)
+
+            # Convert the audio file to mono
+            #  By passing `always_2d` above, we ensure that the audio is always a 2D array regardless of if the
+            #  actual audio is mono or stereo (etc.), which standardises the number of dimensions
+            audio = utils.audio_to_mono(audio)
+
+            # Truncate the audio to the given start time and audio duration
+            audio = utils.truncate_audio(audio, self.sample_rate, self.event_start, self.duration)
+
+            # Set the attributes correctly
+            self.audio = audio
             self._audio_loaded = True
-            return self.audio
+
+        return self.audio
 
     def to_dict(self) -> dict:
         """
         Returns metadata for this Event as a dictionary.
         """
         return dict(
+            alias=self.alias,
             filename=self.filename,
             filepath=self.filepath,
             class_id=self.class_id,
-            label=self.label,
-            start_timepoint=self.start_timepoint,
-            end_timepoint=self.end_timepoint,
+            class_label=self.class_label,
+            scene_start=self.scene_start,
+            event_start=self.event_start,
             duration=self.duration,
+            # TODO: we need start and end timepoints for the scene + event audio
+            # end_timepoint=self.scene_end,
             snr=self.snr,
-            start_coordinates_absolute=self.start_coordinates_absolute,
-            start_coordinates_relative_cartesian=self.start_coordinates_relative_cartesian,
-            start_coordinates_relative_polar=self.start_coordinates_relative_polar,
             spatial_resolution=self.spatial_resolution,
             spatial_velocity=self.spatial_velocity,
-            end_coordinates_absolute=self.end_coordinates_absolute,
-            end_coordinates_relative_cartesian=self.end_coordinates_relative_cartesian,
-            end_coordinates_relative_polar=self.end_coordinates_relative_polar,
+            start_coordinates=dict(
+                absolute=self.start_coordinates_absolute,
+                relative_cartesian=self.start_coordinates_relative_cartesian,
+                relative_polar=self.start_coordinates_relative_polar,
+            ),
+            end_coordinates=dict(
+                absolute=self.end_coordinates_absolute,
+                relative_cartesian=self.end_coordinates_relative_cartesian,
+                relative_polar=self.end_coordinates_relative_polar,
+            )
         )
---
+++ tests/test_event.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test cases for functionality inside audiblelight/event.py"""
+
+import os
+from typing import Optional
+
+import numpy as np
+import pytest
+
+from audiblelight import utils
+from audiblelight.event import Event
+
+TEST_AUDIO_DIRS = utils.list_deepest_directories(utils.get_project_root() / "tests/test_resources/soundevents")
+TEST_AUDIOS = sorted([os.path.join(xs, x) for xs in TEST_AUDIO_DIRS for x in os.listdir(xs) if x.endswith((".wav", ".mp3"))])
+
+
+@pytest.mark.parametrize("audio_fpath", TEST_AUDIOS[:5])
+def test_create_static_event(audio_fpath: str, oyens_space):
+    oyens_space.add_microphone()
+    oyens_space.add_emitter(alias="test_emitter")
+    emitter = oyens_space.get_emitters("test_emitter")
+    ev = Event(audio_fpath, "test_event", emitters=emitter)
+    assert not ev.is_moving
+    # Coordinates should be set properly
+    assert np.array_equal(ev.start_coordinates_absolute, emitter[0].coordinates_absolute)
+    assert np.array_equal(ev.end_coordinates_absolute, emitter[0].coordinates_absolute)
+    # Should be able to create a dictionary
+    ev_dict = ev.to_dict()
+    for k in ["alias", "duration", "start_coordinates", "end_coordinates", "filepath"]:
+        assert k in ev_dict.keys()
+
+
+@pytest.mark.parametrize(
+    "audio_fpath,duration,start_time",
+    [
+        # These are all music audio files, which we use as they're long
+        (TEST_AUDIOS[6], 0.5, 0.5),
+        (TEST_AUDIOS[7], 1.0, 1.0),
+        (TEST_AUDIOS[8], None, 1.0),
+        (TEST_AUDIOS[6], 0.5, None),
+        (TEST_AUDIOS[7], None, None),
+    ]
+)
+def test_load_audio(audio_fpath: str, duration: Optional[float], start_time: Optional[float], oyens_space):
+    # Create a dummy event
+    oyens_space.add_microphone()
+    oyens_space.add_emitter(alias="test_emitter")
+    emitter = oyens_space.get_emitters("test_emitter")
+    ev = Event(
+        audio_fpath,
+        "test_event",
+        emitters=emitter,
+        duration=duration,
+        event_start=start_time,
+        sample_rate=utils.SAMPLE_RATE
+    )
+    # Try and load the audio
+    audio = ev.load_audio(ignore_cache=True)
+    assert isinstance(audio, np.ndarray)
+    assert audio.ndim == 1    # should be mono
+    assert ev._audio_loaded
+    # Try and load the audio again, should be cached
+    audio2 = ev.load_audio(ignore_cache=False)
+    assert np.array_equal(audio, audio2)
+    # If we've passed in a custom duration, this should be respected
+    if duration is not None:
+        assert len(audio) / utils.SAMPLE_RATE == duration
