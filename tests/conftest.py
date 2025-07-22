0d82ec810362af4034fe81667317f8d98d91cfa2
huwcheston <hwc31@cam.ac.uk>
2025-07-22 13:18:58 +0100

    add a `Scene` fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,14 @@
 
 import pytest
 
+from scipy import stats
+
+from audiblelight.core import Scene
 from audiblelight.worldstate import WorldState
 from audiblelight import utils
 
 OYENS_MESH = utils.get_project_root() / "tests/test_resources/meshes/Oyens.glb"
+SOUNDEVENT_DIR = utils.get_project_root() / "tests/test_resources/soundevents"
 
 
 @pytest.fixture(scope="function")
@@ -21,3 +25,21 @@ def oyens_space() -> WorldState:
         empty_space_around_surface=0.2    # all in meters
     )
     return space
+
+
+@pytest.fixture(scope="function")
+def oyens_scene() -> Scene:
+    # Create a dummy scene
+    return Scene(
+        duration=50,
+        mesh_path=OYENS_MESH,
+        mic_arrays=["ambeovr"],
+        # Pass some default distributions for everything
+        event_start_dist=stats.uniform(0, 10),
+        event_duration_dist=stats.uniform(0, 10),
+        event_velocity_dist=stats.uniform(0, 10),
+        event_resolution_dist=stats.uniform(0, 10),
+        snr_dist=stats.norm(5, 1),
+        fg_path=SOUNDEVENT_DIR,
+        max_overlap=1    # no overlapping sound events allowed
+    )
