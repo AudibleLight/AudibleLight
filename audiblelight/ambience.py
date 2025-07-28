106c488b6b4ad608230c81cef9a814eb6f5e7a38
huwcheston <hwc31@cam.ac.uk>
2025-07-28 15:51:24 +0100

    add `Ambience` class and tests

---
+++ audiblelight/ambience.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Generate background noise for a Scene according to a given colour (white, pink...) or value of β.
+
+The core functionality is adapted from [`colorednoise` by Felix Patzelt](https://github.com/felixpatzelt/colorednoise)
+which is released under a permissive MIT license.
+"""
+
+from typing import Any, Union, Iterable, Optional
+
+import numpy as np
+
+from audiblelight import utils
+
+
+# This dictionary maps popular "names" to β values for generating noise
+#  In general, higher β values cause more energy in high frequency parts of the power spectral density
+NOISE_MAPPING = dict(
+    pink=1,
+    brown=2,
+    red=2,
+    blue=-1,
+    white=0,
+    violet=-2
+)
+
+
+class Ambience:
+    """
+    Represents persistent background noise for a Scene.
+
+    Currently, only "colored" forms of noise (white, blue, red, etc.) are supported, with an arbitrary channel count.
+
+    Arguments:
+        shape (tuple): the shape of generated noise, in the form (channels, samples)
+        color (str): the type of noise to generate, e.g. "white", "red", must be provided if `exponent` is None
+        exponent (Numeric): the coefficient for the generated noise, must be provided if `color` is None
+        kwargs: additional values passed to `powerlaw_psd_gaussian`.
+    """
+
+    # noinspection PyTypeChecker
+    def __init__(
+            self,
+            shape: tuple[int, int],
+            color: Optional[str] = None,
+            exponent: Optional[utils.Numeric] = None,
+            **kwargs
+    ):
+        # Parse the exponent for the noise generation
+        #  This can either be a color (e.g., "pink", "white") or a numeric value
+        self.beta = _parse_beta(color, exponent)
+
+        # Validate shape for the noise
+        if len(shape) != 2:
+            raise ValueError(f"Expected `shape` in the form (n_channels, n_samples), but got {shape} instead!")
+        self.shape = shape
+
+        # Validate arguments passed to noise generation function and store them
+        utils.validate_kwargs(powerlaw_psd_gaussian, **kwargs)
+        self.noise_kwargs = kwargs
+
+    def load_ambience(self) -> np.ndarray:
+        """
+        Load the background ambience as an array with shape (channels, samples).
+        """
+        return powerlaw_psd_gaussian(self.beta, self.shape, **self.noise_kwargs)
+
+    def to_dict(self) -> dict:
+        """
+        Returns metadata for this object as a dictionary
+        """
+        return dict(
+            beta=self.beta,
+            shape=self.shape,
+            **self.noise_kwargs
+        )
+
+
+def powerlaw_psd_gaussian(
+        beta: utils.Numeric,
+        shape: Union[int, Iterable[int]],
+        fmin: Optional[utils.Numeric] = 0.0,
+        seed: Optional[int] = utils.SEED,
+        normalize: bool = True
+) -> np.ndarray:
+    """Generate Gaussian (1 / f) ** β noise.
+
+    Based on: Timmer, J. and Koenig, M.: On generating power law noise. Astron. Astrophys. 300, 707-710 (1995)
+
+    Arguments:
+        beta (float): The power-spectrum of the generated noise is proportional to S(f) = (1 / f) ** β,
+        shape (int or iterable): The output has the given shape, and the desired power spectrum in the last coordinate.
+            That is, the last dimension is taken as time, and all other components are independent.
+        fmin (float): Low-frequency cutoff. Default: 0 corresponds to original paper. The largest possible
+            value is fmin = 0.5, the Nyquist frequency. The output for this value is white noise.
+        seed (int): Seed to use when creating the normal distribution.
+        normalize (bool): whether to normalize to unit variance, defaults to True.
+    
+    Returns:
+        np.ndarray: the noise samples in the shape (channels, samples)
+
+    Examples:
+        # Generate monophonic pink noise with 5 samples
+        >>> noise = powerlaw_psd_gaussian(1, 5)
+        >>> noise.shape
+        (5,)
+
+        # Generate quadraphonic pink noise with 10 samples
+        >>> noise = powerlaw_psd_gaussian(1, (4, 10))
+        >>> noise.shape
+        (4, 10)
+    """
+
+    # Make sure size is a list so we can iterate it and assign to it.
+    if isinstance(shape, (np.integer, int)):
+        size = [shape]
+    elif isinstance(shape, Iterable):
+        size = list(shape)
+    else:
+        raise ValueError(f"Argument `shape` must be of type int or Iterable[int] but got {type(shape)}")
+
+    # The number of samples in each time series
+    samples = size[-1]
+
+    # Calculate Frequencies (we assume a sample rate of one)
+    #  Use fft functions for real output (-> hermitian spectrum)
+    f = np.fft.rfftfreq(samples)  # type: ignore # mypy 1.5.1 has problems here
+
+    # Validate / normalise fmin
+    fmin = utils.sanitise_positive_number(fmin)
+    if 0 <= fmin <= 0.5:
+        fmin = max(fmin, 1. / samples)  # Low frequency cutoff
+    else:
+        raise ValueError(f"Argument `fmin` must be chosen between 0 and 0.5 but got {fmin:.2f}.")
+
+    # Build scaling factors for all frequencies
+    s_scale = f
+    ix = np.sum(s_scale < fmin)  # Index of the cutoff
+    if ix and ix < len(s_scale):
+        s_scale[:ix] = s_scale[ix]
+    s_scale = s_scale ** (-beta / 2.)
+
+    # Calculate theoretical output standard deviation from scaling
+    w = s_scale[1:].copy()
+    w[-1] *= (1 + (samples % 2)) / 2.  # correct f = +-0.5
+    sigma = 2 * np.sqrt(np.sum(w ** 2)) / samples
+
+    # Adjust size to generate one Fourier component per frequency
+    size[-1] = len(f)
+
+    # Add empty dimension(s) to broadcast s_scale along last
+    #  dimension of generated random power + phase (below)
+    dims_to_add = len(size) - 1
+    s_scale = s_scale[(np.newaxis,) * dims_to_add + (Ellipsis,)]
+
+    # Prepare random number generator
+    random_state = np.random.default_rng(seed)
+    normal_dist = random_state.normal
+
+    # Generate scaled random power + phase
+    sr = normal_dist(scale=s_scale, size=size)
+    si = normal_dist(scale=s_scale, size=size)
+
+    # If the signal length is even, frequencies +/- 0.5 are equal
+    # so the coefficient must be real.
+    if not (samples % 2):
+        si[..., -1] = 0
+        sr[..., -1] *= np.sqrt(2)  # Fix magnitude
+
+    # Regardless of signal length, the DC component must be real
+    si[..., 0] = 0
+    sr[..., 0] *= np.sqrt(2)  # Fix magnitude
+
+    # Combine power + corrected phase to Fourier components
+    s = sr + 1J * si
+
+    # Transform to real time series & scale to unit variance
+    y = np.fft.irfft(s, n=samples, axis=-1)
+    if normalize:
+        y /= sigma
+
+    return y
+
+
+def _parse_beta(color: Any, exponent: Any) -> float:
+    """
+    Parses the noise exponential term from either a string representation of a color (white) or a number.
+    """
+    # Both values are provided
+    if color is not None and exponent is not None:
+        # The provided color does not have the same exponent as the provided value
+        if color in NOISE_MAPPING.keys() and NOISE_MAPPING[color] != exponent:
+            raise ValueError("Both `color` and `exponent` were provided, however the values do not match: "
+                             f"expected {NOISE_MAPPING[color]}, but got {exponent}.")
+        # else: use "color" not "exponent", but it doesn't really matter as both would give the same results
+
+    # String color must be in the dictionary
+    if color is not None:
+        if color in NOISE_MAPPING.keys():
+            return NOISE_MAPPING[color]
+        elif not isinstance(color, str):
+            raise TypeError(f"`color` must be a string but got {type(color)}")
+        else:
+            keys = ", ".join(k for k in NOISE_MAPPING.keys())
+            raise KeyError(f"`color` must be a string in {keys} but got {color}.")
+
+    # Otherwise, exponent must be numeric
+    elif exponent is not None:
+        if isinstance(exponent, utils.Numeric):
+            return exponent
+        else:
+            raise TypeError(f"`exponent` must be a numeric value, but got {type(exponent)}")
+
+    # Must provide either a color or exponent
+    else:
+        raise TypeError("Either one of `color` or `exponent` must be provided.")
---
+++ tests/test_ambience.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test cases for functionality inside audiblelight/ambience.py, many of which are adapted from `colorednoise`"""
+
+import numpy as np
+import pytest
+
+from audiblelight.ambience import powerlaw_psd_gaussian, _parse_beta, Ambience
+
+
+@pytest.mark.parametrize("shape",[2, 16, 500, 1000])
+def test_powerlaw_psd_gaussian_scalar_output_shape(shape):
+    n = powerlaw_psd_gaussian(1, shape)
+    assert n.shape == (shape,)
+
+
+@pytest.mark.parametrize("shape", [(1, 1000), (6, 600), (4, 44100), (8, 100000)])
+def test_powerlaw_psd_gaussian_vector_output_shape(shape):
+    n = powerlaw_psd_gaussian(1, shape)
+    assert n.shape == shape
+
+
+def test_powerlaw_psd_gaussian_output_finite():
+    n = powerlaw_psd_gaussian(1, 16, fmin=0.1)
+    assert np.isfinite(n).all()
+
+
+@pytest.mark.parametrize("exponent", [0.5, 1, 2])
+def test_var_distribution(exponent):
+    size = (100, 2 ** 16)
+    fmin = 0
+    y = powerlaw_psd_gaussian(exponent, size, fmin=fmin, seed=1)
+    ystd = y.std(axis=-1)
+    var_in = (abs(1 - ystd) < 3 * ystd.std()).mean()
+    assert var_in > 0.95
+
+
+@pytest.mark.parametrize("nsamples", [10, 11])
+def test_small_sample_var(nsamples):
+    ystd = powerlaw_psd_gaussian(0, (500, 500, nsamples), seed=1).std(axis=-1)
+    assert (abs(1 - ystd) < 3 * ystd.std()).mean() > 0.95
+
+
+@pytest.mark.parametrize("exponent", [0.5, 1, 2])
+def test_slope_distribution(exponent):
+    size = (100, 2 ** 16)
+    fmin = 0
+    y = powerlaw_psd_gaussian(exponent, size, fmin=fmin, seed=1)
+    yfft = np.fft.fft(y)
+    f = np.fft.fftfreq(y.shape[-1])
+    m = f > 0
+    fit, fcov = np.polyfit(np.log10(f[m]), np.log10(np.abs(yfft[..., m].T ** 2)),1, cov=True)
+    slope_in = (exponent + fit[0] < 3 * np.sqrt(fcov[0, 0])).mean()
+    assert slope_in > 0.95
+
+
+def test_cumulative_scaling():
+    n_repeats = 1000
+    n_steps = 100
+    y = powerlaw_psd_gaussian(0, (n_repeats, n_steps), seed=1)
+    mean_squared_displacement = (y.sum(axis=-1) ** 2).mean(axis=0)
+    standard_error = (y.sum(axis=-1) ** 2).std(axis=0) / np.sqrt(n_repeats)
+    assert abs(n_steps - mean_squared_displacement) < 3 * standard_error
+
+
+def test_random_state_reproducibility():
+    exp = 1
+    n = 5
+    y1 = powerlaw_psd_gaussian(exp, n, seed=1)
+    np.random.seed(123)  # Reset global RNG to make sure it doesn’t interfere
+    y2 = powerlaw_psd_gaussian(exp, n, seed=1)
+    np.testing.assert_array_equal(y1, y2)
+
+
+@pytest.mark.parametrize(
+    "color, exponent, expected",
+    [
+        ("white", None, 0),
+        ("pink", None, 1),
+        ("brown", None, 2),
+        ("not-a-color", None, KeyError),
+        (None, 1.5, 1.5),
+        (None, 0, 0),
+        (None, np.float64(2.0), 2.0),
+        (123, None, TypeError),
+        (None, "high", TypeError),
+        (None, None, TypeError),
+        ("white", -11, ValueError)   # given noise color does not have expected beta
+    ]
+)
+def test_parse_beta(color, exponent, expected):
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            _parse_beta(color, exponent)
+    else:
+        assert _parse_beta(color, exponent) == expected
+
+
+@pytest.mark.parametrize(
+    "shape, color, exponent",
+    [
+        ((4, 2000), "white", None),
+        ((4, 2000), None, 2.0),
+    ]
+)
+def test_ambience_cls(shape, color, exponent):
+    cls = Ambience(shape, color, exponent)
+    assert isinstance(cls.to_dict(), dict)
+    assert cls.load_ambience().shape == shape
