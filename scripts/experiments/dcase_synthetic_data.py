8383847e102be919d10273256fc834c7b23b1e27
huwcheston <hwc31@cam.ac.uk>
2025-10-15 11:21:17 +0100

    add CLI interface for data generation

--- scripts/experiments/dcase_synthetic_data.py
+++ scripts/experiments/dcase_synthetic_data.py
@@ -37,6 +37,7 @@ from audiblelight import config, utils
 from audiblelight.augmentation import Distortion, Invert, PitchShift, Reverse, SpeedUp
 from audiblelight.core import Scene
 from audiblelight.worldstate import MATERIALS_JSON
+from scripts.experiments.dcase_selected_meshes import MESHES
 
 # For reproducible randomisation
 utils.seed_everything(utils.SEED)
@@ -45,20 +46,6 @@ utils.seed_everything(utils.SEED)
 FG_DIR = utils.get_project_root() / "resources/soundevents"
 MESH_DIR = utils.get_project_root() / "resources/meshes/gibson"
 OUTPUT_DIR = utils.get_project_root() / "spatial_scenes_dcase_synthetic"
-
-# Data splits
-TRAIN_RECORDINGS_PER_ROOM, TEST_RECORDINGS_PER_ROOM = 150, 100
-# Randomly sampled, but hardcoded so they are the same across runs
-TRAIN_ROOMS = [
-    "Haymarket.glb",
-    "Swisshome.glb",
-    "Siren.glb",
-    "Traver.glb",
-    "Hercules.glb",
-    "Halfway.glb",
-]
-TEST_ROOMS = ["Helix.glb", "Peacock.glb", "Vails.glb"]
-
 
 # Parameters taken from DCASE data
 DURATION = 60
@@ -80,7 +67,14 @@ VALID_MATERIALS = list({mat["name"] for mat in js_out["materials"]})
 
 
 def generate(
-    mesh_name: str, split: str, scene_num: int, scape_num: int, output_dir: Path
+    mesh_name: str,
+    split: str,
+    scene_num: int,
+    scape_num: int,
+    output_dir: Path,
+    channel_layout: str,
+    augmentations: bool,
+    materials: bool,
 ) -> None:
     """
     Make a single generation with required arguments
@@ -91,21 +85,44 @@ def generate(
     # Output filepaths
     fold = 1 if split == "train" else 2
     common = f"dev-{split}-alight/fold{fold}_scene{scene_num}_{str(scape_num).zfill(3)}"
-    audio_path = output_dir / f"mic_dev/{common}.wav"
+    audio_path = output_dir / f"{channel_layout}_dev/{common}.wav"
     metadata_path = output_dir / f"metadata_dev/{common}.csv"
 
     # Skip over this generation if files already exist
     if (
-        audio_path.with_name(audio_path.stem + "_mic.wav").exists()
-        and metadata_path.with_name(metadata_path.stem + "_mic.csv").exists()
+        audio_path.with_name(audio_path.stem + f"_{channel_layout}.wav").exists()
+        and metadata_path.with_name(
+            metadata_path.stem + f"_{channel_layout}.csv"
+        ).exists()
     ):
         return
 
     # Choose a material to use
-    scene_material = random.choice(VALID_MATERIALS)
+    if materials:
+        use_material = random.choice(VALID_MATERIALS)
+    else:
+        use_material = "Default"
 
     # Choose a noise floor for the scene
     scene_ref_db = np.random.uniform(config.MIN_REF_DB, config.MAX_REF_DB)
+
+    # Use augmentations
+    if augmentations:
+        use_augmentations = [
+            Reverse,
+            Invert,
+            (PitchShift, dict(sample_rate=SAMPLE_RATE, semitones=stats.uniform(-7, 0))),
+            (
+                SpeedUp,
+                dict(sample_rate=SAMPLE_RATE, stretch_factor=stats.uniform(0.9, 0.2)),
+            ),
+            (
+                Distortion,
+                dict(sample_rate=SAMPLE_RATE, drive_db=stats.uniform(0.0, 10.0)),
+            ),
+        ]
+    else:
+        use_augmentations = None
 
     scene = Scene(
         duration=DURATION,
@@ -133,25 +150,13 @@ def generate(
             config.MIN_EVENT_SNR, config.MAX_EVENT_SNR - config.MIN_EVENT_SNR
         ),
         # Event augmentations will sample from this list
-        event_augmentations=[
-            Reverse,
-            Invert,
-            (PitchShift, dict(sample_rate=SAMPLE_RATE, semitones=stats.uniform(-7, 0))),
-            (
-                SpeedUp,
-                dict(sample_rate=SAMPLE_RATE, stretch_factor=stats.uniform(0.9, 0.2)),
-            ),
-            (
-                Distortion,
-                dict(sample_rate=SAMPLE_RATE, drive_db=stats.uniform(0.0, 10.0)),
-            ),
-        ],
+        event_augmentations=use_augmentations,
         fg_path=Path(FG_DIR),
         max_overlap=MAX_OVERLAP,
         ref_db=scene_ref_db,
         state_kwargs=dict(
             add_to_context=False,
-            material=scene_material,
+            material=use_material,
             rlr_kwargs=dict(sample_rate=SAMPLE_RATE),
         ),
         allow_duplicate_audios=False,
@@ -159,12 +164,15 @@ def generate(
 
     # Add the microphone, static + moving events (one augmentation sampled randomly from above list)
     #  skip over any errors when adding the event and just continue to the next one
-    scene.add_microphone(microphone_type=config.MIC_ARRAY_TYPE, alias="mic")
+    scene.add_microphone(
+        microphone_type="ambeovr" if channel_layout == "mic" else "foalistener",
+        alias=channel_layout,
+    )
     for _ in range(STATIC_EVENTS.rvs()):
         try:
             scene.add_event(
                 event_type="static",
-                augmentations=1,
+                augmentations=1 if augmentations else None,
                 ensure_direct_path=True,
                 max_place_attempts=100,
             )
@@ -177,7 +185,7 @@ def generate(
         try:
             scene.add_event(
                 event_type="moving",
-                augmentations=1,
+                augmentations=1 if augmentations else None,
                 ensure_direct_path=True,
                 max_place_attempts=100,
                 shape=shape,
@@ -196,6 +204,9 @@ def generate(
             scene_num=scene_num,
             scape_num=scape_num,
             output_dir=output_dir,
+            channel_layout=channel_layout,
+            augmentations=augmentations,
+            materials=materials,
         )
 
     # Do the generation: create audio and DCASE metadata
@@ -212,35 +223,70 @@ def generate(
     fig.savefig(metadata_path.with_suffix(".png").as_posix())
 
 
-def main(outdir: str):
+def main(
+    channel_layout: str, augmentations: bool, materials: bool, meshes: int, outdir: str
+):
+    # Parse the channel layout and microphone type
+    if channel_layout not in ["mic", "foa"]:
+        raise ValueError(
+            "Expected channel_layout 'mic' or 'foa' but got {}".format(channel_layout)
+        )
+
     # Create the output folders if they don't currently exist
     outdir = Path(outdir)
     for fp in [
         outdir / "metadata_dev/dev-train-alight",
         outdir / "metadata_dev/dev-test-alight",
-        outdir / "mic_dev/dev-train-alight",
-        outdir / "mic_dev/dev-test-alight",
+        outdir / f"{channel_layout}_dev/dev-train-alight",
+        outdir / f"{channel_layout}_dev/dev-test-alight",
     ]:
         if not fp.exists():
             os.makedirs(fp)
 
+    # Get the train + test meshes for this run
+    if str(meshes) not in MESHES.keys():
+        raise ValueError(f"Expected meshes in {list(MESHES.keys())} but got {meshes}")
+    chosen = MESHES[str(meshes)]
+    train_rooms = chosen["train"]
+    test_rooms = chosen["test"]
+    train_recordings_per_room = chosen["scapes_per_train_mesh"]
+    test_recordings_per_room = chosen["scapes_per_test_mesh"]
+
     # Start iterating to create the required number of training scenes
     logger.info("Generating training scenes...")
     full_start = time()
-    for train_room_idx, train_room in enumerate(TRAIN_ROOMS):
+    for train_room_idx, train_room in enumerate(train_rooms):
         for train_scape_idx in tqdm(
-            range(TRAIN_RECORDINGS_PER_ROOM),
-            desc=f"Generating for train room {train_room_idx}...",
+            range(train_recordings_per_room),
+            desc=f"Generating for train room {train_room_idx + 1}/{len(train_rooms)}, name {train_room}...",
         ):
-            generate(train_room, "train", train_room_idx, train_scape_idx, outdir)
+            generate(
+                train_room,
+                "train",
+                train_room_idx,
+                train_scape_idx,
+                outdir,
+                channel_layout,
+                augmentations,
+                materials,
+            )
 
     logger.info("Generating testing scenes...")
-    for test_room_idx, test_room in enumerate(TEST_ROOMS):
+    for test_room_idx, test_room in enumerate(test_rooms):
         for test_scape_idx in tqdm(
-            range(TEST_RECORDINGS_PER_ROOM),
-            desc=f"Generating for test room {test_room_idx}...",
+            range(test_recordings_per_room),
+            desc=f"Generating for test room {test_room_idx + 1}/{len(test_rooms)}, name {test_room}...",
         ):
-            generate(test_room, "test", test_room_idx, test_scape_idx, outdir)
+            generate(
+                test_room,
+                "test",
+                test_room_idx,
+                test_scape_idx,
+                outdir,
+                channel_layout,
+                augmentations,
+                materials,
+            )
 
     # Log the time taken
     full_end = time() - full_start
@@ -252,11 +298,32 @@ if __name__ == "__main__":
         description="Generates similar synthetic data to https://zenodo.org/records/6406873 using AudibleLight"
     )
     parser.add_argument(
+        "--channel-layout",
+        type=str,
+        default=config.DEFAULT_CHANNEL_LAYOUT,
+        help="The channel layout to use, must be either 'foa' or 'mic'",
+    )
+    parser.add_argument(
+        "--augmentations",
+        action="store_true",
+        help="Add this flag to use augmentations",
+    )
+    parser.add_argument(
+        "--materials", action="store_true", help="Add this flag to use materials"
+    )
+    parser.add_argument(
+        "--meshes",
+        type=int,
+        default=9,
+        help="The number of meshes to use. Note that the total number of scapes to generate will remain fixed at 1200.",
+    )
+    parser.add_argument(
         "--outdir",
         type=int,
         default=OUTPUT_DIR,
         help=f"Path to save generated outputs, defaults to {OUTPUT_DIR}",
     )
     args = vars(parser.parse_args())
+    logger.info("Generating with args: {}".format(args))
 
     main(**args)
